0d759b641d740418ce523fc7b5d3795f9068ca53
fndillah <affanfatan33@gmail.com>
2024-05-02 10:36:42 +0700

    cek

--- predict_view.py
+++ predict_view.py
@@ -7,8 +7,15 @@ diabetes_model = pickle.load(open('diabetes_model.sav',"rb"))
 st.title('Sistem Pembuat Keputusan Diabetes Menggunakan Decision Tree')
 st.sidebar.header('Variable input')
 
+user_input = st.sidebar.text_input("Pregnancies")
 
-pregnancies = st.sidebar.slider('Pregnancies',  min_value=0,  max_value=20,  step=1)
+# Convert the user input to an integer if it's a valid number
+if user_input.isdigit():
+    slider_value = int(user_input)
+else:
+    slider_value = 0  # Default value if the input is not a valid number
+
+pregnancies = st.sidebar.slider('Pregnancies',  min_value=0,  max_value=20,  step=1, value=slider_value)
 glucose = st.sidebar.slider('Glucose', min_value=60, max_value=240, step=1)
 blood_pressure = st.sidebar.slider('Blood Pressure', min_value=0, max_value=140, step=1)
 skin_thickness = st.sidebar.slider('Skin Thickness',  min_value=0,  max_value=100,  step=1)
