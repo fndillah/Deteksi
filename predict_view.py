8e009ca073ae8fdf1626bf819e7caf35ab0266bd
muhammadandikasyptr <2110631250013@student.unsika.ac.id>
2024-05-16 11:42:04 +0700

    update sistem pertama

--- predict_view.py
+++ predict_view.py
@@ -11,13 +11,13 @@ st.title('Sistem Penunjang Keputusan Deteksi Diabetes')
 # Input pengguna
 st.header('Masukkan Parameter Kesehatan:')
 pregnancies = st.number_input('Jumlah Kehamilan', min_value=0, max_value=20, value=0)
-glucose = st.number_input('Kadar Glukosa', min_value=0, max_value=200, value=0)
-blood_pressure = st.number_input('Tekanan Darah Diastolik (mm Hg)', min_value=0, max_value=150, value=0)
+glucose = st.number_input('Kadar Glukosa', min_value=0, max_value=240, value=0)
+blood_pressure = st.number_input('Tekanan Darah Diastolik (mm Hg)', min_value=0, max_value=140, value=0)
 skin_thickness = st.number_input('Ketebalan Lipatan Kulit (mm)', min_value=0, max_value=100, value=0)
-insulin = st.number_input('Insulin (mu U/ml)', min_value=0, max_value=900, value=0)
-bmi = st.number_input('Indeks Massa Tubuh (BMI)', min_value=0.0, max_value=70.0, value=0.0)
-dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=3.0, value=0.0)
-age = st.number_input('Usia', min_value=0, max_value=120, value=0)
+insulin = st.number_input('Insulin (mu U/ml)', min_value=0, max_value=1000, value=0)
+bmi = st.number_input('Indeks Massa Tubuh (BMI)', min_value=0.0, max_value=200.0, value=0.00)
+dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=3.0, value=0.00)
+age = st.number_input('Usia', min_value=0, max_value=100, value=0)
 
 # Tombol prediksi
 if st.button('Prediksi Risiko Diabetes'):
